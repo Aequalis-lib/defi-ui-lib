a27aaec3cac48b2fdfa0ded0e8943163febb7772
Thanush Praveen <thanushpraveen@aequalisys.com>
2021-07-23 13:53:06 +0530

    TransactionModal setup

---
+++ src/index.tsx
@@ -0,0 +1,147 @@
+import * as React from 'react';
+import { Modal } from 'react-bootstrap';
+import Loader from 'react-loader-spinner';
+import Alert from './error.svg';
+// Delete me
+const TransactionModal: React.FC = (props: any) => {
+  const [mode, setMode] = React.useState<string>('success');
+
+  function transactionMethods() {
+    switch (mode) {
+      case 'success':
+        return (
+          <>
+            <Modal
+              className={`modal-transaction-${props.theme}`}
+              centered
+              {...props}
+            >
+              <Modal.Header className="modal-header-custom" closeButton>
+                <Modal.Title className="model-title-custom"></Modal.Title>
+              </Modal.Header>
+              <Modal.Body className="custom-modal-body">
+                <div className="modal-body-info">
+                  <div className="pb-1 pt-0">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="90"
+                      height="90"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      stroke="#2a6def"
+                      style={{
+                        strokeWidth: '0.5',
+                        strokeLinecap: 'round',
+                        strokeLinejoin: 'round',
+                      }}
+                    >
+                      <circle cx="12" cy="12" r="10"></circle>
+                      <polyline points="16 12 12 8 8 12"></polyline>
+                      <line x1="12" y1="16" x2="12" y2="8"></line>
+                    </svg>
+                  </div>
+
+                  <div className="transaction-details">
+                    <div className=" transaction-status ">
+                      Transaction Submitted
+                    </div>
+                    <a
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      href="/viewonetherscan"
+                      className="sc-jKJlTe cEMwVc"
+                    >
+                      <div className="etherscan-link">View on Explorer</div>
+                    </a>
+                  </div>
+                  <button
+                    className="custom-transaction-btn btn btn-primary mt-4"
+                    onClick={props.onHide}
+                    type="button"
+                  >
+                    Close
+                  </button>
+                </div>
+              </Modal.Body>
+            </Modal>
+          </>
+        );
+      case 'loading':
+        return (
+          <>
+            <Modal
+              className={`modal-transaction-${props.theme}`}
+              {...props}
+              centered
+              animation={false}
+              size="md"
+            >
+              <Modal.Header className="modal-header-custom" closeButton>
+                <Modal.Title className="model-title-custom"></Modal.Title>
+              </Modal.Header>
+              <Modal.Body>
+                <div className="modal-body-info">
+                  <div
+                    style={{
+                      margin: ' 25px 0',
+                    }}
+                  >
+                    <Loader
+                      type="Circles"
+                      color={`${
+                        props.theme === 'dark' ? '#800080' : '#800080'
+                      }`}
+                      height={100}
+                      width={100}
+                    />
+                  </div>
+                  <h4 className="mt-4">Waiting For Confirmation</h4>
+                  <p>Confirm this transaction in your wallet</p>
+                </div>
+              </Modal.Body>
+            </Modal>
+          </>
+        );
+      case 'failure':
+        return (
+          <>
+            <Modal
+              className={`modal-transaction-${props.theme}`}
+              {...props}
+              centered
+              animation={false}
+              size="md"
+            >
+              <Modal.Header className="modal-header-custom" closeButton>
+                <Modal.Title className="model-title-custom">Error</Modal.Title>
+              </Modal.Header>
+              <Modal.Body>
+                <div className="modal-body-info">
+                  <img
+                    className="icon"
+                    src={Alert}
+                    alt="alert icon"
+                    width="85"
+                  />
+                  <h5 className="mt-4">Transaction Rejected</h5>
+                  <button
+                    className="btn btn-primary custom-transaction-btn"
+                    onClick={props.onHide}
+                    type="button"
+                  >
+                    Close
+                  </button>
+                </div>
+              </Modal.Body>
+            </Modal>
+          </>
+        );
+      default:
+        return <></>;
+    }
+  }
+
+  return <div>{transactionMethods()}</div>;
+};
+
+export default TransactionModal;
